128c4e928a441d31189d35e5c698fb8f868708d1
João Marcos <joaombcerqueira@gmail.com>
2023-03-21 14:54:51 -0300

    creating types

---
+++ 3/main.go
@@ -0,0 +1,18 @@
+package main
+
+const a = "Hello World"
+
+type ID int // Create Types
+
+var (
+	b bool 		= true 
+	c int  		= 10 
+	d string 	= "Joao" 
+	e float64 	= 1.2 
+	f ID 		= 1
+)
+ 
+func main() {
+	println(f) 
+
+}
