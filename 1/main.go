44374c7291e7efa3cea51793952fa349a55560e4
João Marcos <joaombcerqueira@gmail.com>
2023-03-21 13:15:04 -0300

    first

---
+++ 1/main.go
@@ -0,0 +1,11 @@
+package main
+
+const a = "Hello World"
+
+func main() {
+	println("Hello, GO!")
+	println(a)
+}
+
+// If is in the same package, can be reached by any file
+// Every file in the same directory must have the same package name
