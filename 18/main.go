874b5c5b182547851fe8dacf25ff8a898fa23312
João Marcos <joaombcerqueira@gmail.com>
2023-03-30 18:16:26 -0300

    empty interfaces

---
+++ 18/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x interface{} = 10
+	var y interface{} = "Hello World"
+
+	showType(x)
+	showType(y)
+
+}
+
+func showType(t interface{}) {
+	fmt.Printf("Type of var: %T\nValue of var: %v\n", t, t)
+}
