a356a4cb8bd95bdbbceeaad914a00b19fb2a4015
joaomarcosbc <joaombcerqueira@gmail.com>
2023-05-02 15:23:17 -0300

    go get

--- 21/main.go
+++ 21/main.go
@@ -1,9 +1,16 @@
 package main
 
-import "first/math"
+import (
+	"first/math"
+	"fmt"
+
+	"github.com/google/uuid"
+)
 
 func main() {
 	s := math.Sum(10, 20)
+
+	fmt.Println(uuid.New())
 
 	println(s)
 }
